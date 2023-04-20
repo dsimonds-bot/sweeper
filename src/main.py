70e78b2d28091bbbb8efeee99f012217a35cdb50
David Simonds <davesimonds6@gmail.com>
2023-04-20 18:21:47 -0400

    test bypassing data_import in main

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 # Package Import
 import pandas as pd
+import numpy as np
 import util
 import assets
 import data_import
 
-def assets_build(dataframe: pd.DataFrame, save_components = False):
+def assets_build(dataframe: pd.DataFrame):
 
     # Make dataframe attributes
     dataframe_structure = data_import.loading_dock(dataframe);
@@ -23,3 +24,26 @@ def assets_build(dataframe: pd.DataFrame, save_components = False):
 
     # Making aggregate dataframe images
     assets.dataframe_image(dataframe.describe());
+    
+def direct_asset_build(dataframe: pd.DataFrame):
+
+    # Make the boxplots
+    assets.make_boxplots(
+        dataframe.select_dtypes(include = np.number)
+    )
+
+    # Make the histograms
+    assets.make_histograms(
+        dataframe.select_dtypes(include = np.number)
+    )
+
+    # Create the column-wise describe storage
+    storage = util.column_describe_storage(
+        dataframe.select_dtypes(include = np.number)
+    )
+
+    for column_names in list(storage.keys()):
+        assets.dataframe_image(storage[column_names])
+
+    # Making aggregate dataframe images
+    assets.dataframe_image(dataframe.describe())
