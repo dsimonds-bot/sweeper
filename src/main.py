42726d3a7918a74242eda461addab6a93a617b7e
David Simonds <davesimonds6@gmail.com>
2023-04-21 11:55:33 -0400

    remove outdated assets build following test

--- src/main.py
+++ src/main.py
@@ -6,26 +6,6 @@ import assets
 import data_import
 
 # Function Definitions
-def assets_build(dataframe: pd.DataFrame):
-
-    # Make dataframe attributes
-    dataframe_structure = data_import.loading_dock(dataframe);
-
-    # Make the boxplots
-    assets.make_boxplots(dataframe_structure.num());
-
-    # Make the histograms
-    assets.make_histograms(dataframe_structure.num());
-
-    # Create the column-wise describe storage
-    storage = util.column_describe_storage(dataframe_structure.num());
-
-    for column_names in list(storage.keys()):
-        assets.dataframe_image(storage[column_names]);
-
-    # Making aggregate dataframe images
-    assets.dataframe_image(dataframe.describe());
-    
 def direct_assets_build(dataframe: pd.DataFrame):
 
     # Make the boxplots
